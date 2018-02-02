19d0a97a7f6ce14e7a9722e275e09969be6adf57
Juan Biondi <juanernestobiondi@hotmail.com>
2018-02-02 16:15:10 +0100

    Add argument execution.

--- SortRequirementPIP.py
+++ SortRequirementPIP.py
@@ -5,16 +5,52 @@
 EXECUTE THE CODE
 """
 
+def orderPIPFile(pstrFileName):
+    Data = []
+    with open(pstrFileName, "r") as f:
+        for line in f.readlines():
+            Data.append(line.rstrip())
 
-Data = []
-with open("RequirementsPIP.txt", "r") as f:
-    for line in f.readlines():
-        Data.append(line.rstrip())
+    SortedData = sorted(Data, key=str.lower)
+    f.close()
 
-SortedData = sorted(Data, key=str.lower)
-f.close()
+    with open(pstrFileName, "w") as g:
+        for i in range(0, len(SortedData)):
+            g.write(SortedData[i] + "\n")
+    g.close
 
-with open("RequirementsPIP.txt", "w") as g:
-    for i in range(0, len(SortedData)):
-        g.write(SortedData[i] + "\n")
-g.close
+
+
+def main():
+    usage = "usage: %prog [options] <arguments>"
+    parser = OptionParser(usage, version="%prog 0.1")
+    parser.add_option("-i", "--input",
+                      action="store",
+                      type="string", dest="filename",
+                      help="Input file name to order alphabetically the requirements. (This is mandatory")
+    (options, args) = parser.parse_args()
+
+    mandatories = ['filename']
+
+    if len(sys.argv[1:]) == 0:
+        parser.error("\nSeems like there is no enough arguments.\nPlease type {} --help for more information.".format(__file__))
+        parser.print_help()
+        exit(-1)
+    else:
+        for m in mandatories:
+            if not options.__dict__[m]:
+                printError("There are arguments missing.")
+                parser.print_help()
+                exit(-1)
+        if options.filename:
+            print("\nSorting the {} file.\n".format(options.filename))
+            orderPIPFile(options.filename)
+            if repoDir == "":
+                exit()
+            else:
+                print("ERROR!!!")
+        else:
+            printError("You have to specify the input file name.")
+
+if __name__ == "__main__":
+    main()
