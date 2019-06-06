a9ac07320f9ea14951f79e24dd6e1e0bc422c1ef
Juan Biondi <juanernestobiondi@gmail.com>
2019-06-06 08:09:43 +0200

    Create configure.sh

---
+++ Python/jupyter/configure.sh
@@ -0,0 +1,5 @@
+jupyter contrib nbextension install
+jupyter nbextension enable --py widgetsnbextension
+jupyter-nbextension install rise --py --sys-prefix
+jupyter-nbextension enable rise --py --sys-prefix
+jupyter nbextension enable --py --sys-prefix qgrid
