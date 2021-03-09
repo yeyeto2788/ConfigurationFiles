a786560a330409da9cfe205ed32ffd68bc6a93b8
Juan Biondi <juanernestobiondi@gmail.com>
2021-03-09 11:46:13 +0100

    Delete Python/jupyter directory

--- Python/jupyter/configure.sh
+++
@@ -1,5 +0,0 @@
-jupyter contrib nbextension install
-jupyter nbextension enable --py widgetsnbextension
-jupyter-nbextension install rise --py --sys-prefix
-jupyter-nbextension enable rise --py --sys-prefix
-jupyter nbextension enable --py --sys-prefix qgrid
